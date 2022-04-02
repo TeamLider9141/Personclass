ce1cf92dbb9f03337cc12ad2c13fa0b1a21effae
TeamLider9141 <juraqulovortiq2002@gmail.com>
2022-04-03 02:03:46 +0500

    personclass

---
+++ Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_class
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public  DateTime Birthday { get; set; }
+        public float Height { get; set; }
+        public int Weight { get; set; }
+        public double Age()
+        {
+            return (DateTime.Now - Birthday).Days ;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+using static System.Math;
+
+using System.Numerics;
+using first_class;
+
+namespace first_class
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+         
+            Person person = new Person()
+            {
+                FirstName = "Killer",
+                LastName = "9141",
+
+                 Birthday = new DateTime(2002, 02, 02),
+            };
+             Console.WriteLine(person.Age()); //  number of days I lived!!!
+
+
+        }
+    }
+}
+
